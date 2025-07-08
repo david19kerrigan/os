63a0265ef16361cb630a6bdead91c0358312aebe
david19kerrigan <david19kerrigan@gmail.com>
2025-07-08 08:34:29 -0400

    up

--- kernel/kernel.d
+++
@@ -1,6 +0,0 @@
-kernel.o: kernel.c \
- /home/david/Downloads/cc/i386-elf-7.5.0-Linux-x86_64/lib/gcc/i386-elf/7.5.0/include/stdbool.h \
- /home/david/Downloads/cc/i386-elf-7.5.0-Linux-x86_64/lib/gcc/i386-elf/7.5.0/include/stddef.h \
- /home/david/Downloads/cc/i386-elf-7.5.0-Linux-x86_64/lib/gcc/i386-elf/7.5.0/include/stdint.h \
- /home/david/Downloads/cc/i386-elf-7.5.0-Linux-x86_64/lib/gcc/i386-elf/7.5.0/include/stdint-gcc.h \
- /home/david/Documents/os/sysroot/usr/local/include/string.h
